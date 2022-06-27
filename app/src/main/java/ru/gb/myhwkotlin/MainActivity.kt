fd2ec16a28b2c8a72308e35e96157c10db2a0bba
Julia Nikishina <pandora55592@mail.ru>
2022-06-27 16:48:26 +0300

    Добавление кнопки.

--- app/src/main/java/ru/gb/myhwkotlin/MainActivity.kt
+++ app/src/main/java/ru/gb/myhwkotlin/MainActivity.kt
@@ -2,10 +2,29 @@ package ru.gb.myhwkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var closeMe: Button
+    lateinit var textView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        closeMe = findViewById(R.id.close_me)
+        textView = findViewById(R.id.textView)
+
+
+        closeMe.setOnClickListener {
+            Log.i("MainActivity", "Hello World")
+            textView.text = ""
+            for(i in 10 downTo 1 step 2) {
+                textView.append("Hello Kotlin!")
+            }
+        }
     }
 }
